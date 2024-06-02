4af732621041c67b5e606f754efb4094584ff398
Quoc Khanh <nguyenkimt322@gmail.com>
2024-06-02 15:15:20 +0700

    initial commit for helm

--- k3d-cluster-copy/delete.sh
+++ k3d-cluster-copy/delete.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-# Delete all deployments
-kubectl delete deployment --all
+# Create directory structure
+mkdir -p /vagrant/capstone-chart/templates/{api-gateway,email-service,eureka-server,frontend,keycloak,mongodb,movie-service,postgres,redis,user-service,zipkin,zookeeper}
 
-# Delete all services
-kubectl delete service --all
+# Create YAML files for each service
+declare -A services=(
+  ["api-gateway"]="api-gateway-deployment.yaml api-gateway-service.yaml"
+  ["email-service"]="email-service-deployment.yaml email-service-service.yaml"
+  ["eureka-server"]="eureka-server-deployment.yaml eureka-server-service.yaml"
+  ["frontend"]="frontend-deployment.yaml frontend-service.yaml"
+  ["keycloak"]="keycloak-deployment.yaml keycloak-service.yaml"
+  ["mongodb"]="mongodb-deployment.yaml mongodb-pvc.yaml mongodb-pv.yaml mongodb-service.yaml"
+  ["movie-service"]="movie-service-deployment.yaml movie-service-service.yaml"
+  ["postgres"]="postgres-deployment.yaml postgres-pvc.yaml postgres-pv.yaml postgres-service.yaml"
+  ["redis"]="redis-deployment.yaml redis-service.yaml"
+  ["user-service"]="user-service-deployment.yaml user-service-service.yaml"
+  ["zipkin"]="zipkin-deployment.yaml zipkin-service.yaml"
+  ["zookeeper"]="zookeeper-deployment.yaml zookeeper-service.yaml"
+)
 
-# Delete all persistent volume claims
-kubectl delete pvc --all
+# Loop through the services array and create the files
+for service in "${!services[@]}"; do
+  for file in ${services[$service]}; do
+    touch /vagrant/capstone-chart/templates/$service/$file
+  done
+done
 
-kubectl delete pv --all
+echo "All files created successfully."
